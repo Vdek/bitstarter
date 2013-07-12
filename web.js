2e202fdf97aeedfde060e6943d2c3a8535673e53
Jurgen Shestani <jurgenshestani@gmail.com>
2013-07-12 03:45:58 +0000

    updated web.js once again, but fixed this time I swear

--- web.js
+++ web.js
@@ -2,13 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(256);
-
-var text = fs.readFileSync('index.html', 'utf8');
+var buf = new Buffer(fs.readFileSync('index.html', 'utf8'), "utf-8");
 
 
 app.get('/', function(request, response) {
-  response.send(Buffer.toString(text, "utf-8"));
+  response.send(buf.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
