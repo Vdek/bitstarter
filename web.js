fafac6488a34f2797ef14c46f48a66a2faebc661
Jurgen Shestani <jurgenshestani@gmail.com>
2013-08-08 05:19:00 +0000

    added social buttons

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 
-var app = express.createServer(express.logger());
+var app = express();
 
 //var buf = new Buffer(fs.readFileSync('index.html', 'utf8'), "utf-8");
 
